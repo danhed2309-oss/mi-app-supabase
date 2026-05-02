390bfcc8b3807b6c94c2194aab8fc092db4d0a87
danhed2309-oss <danhed2309@gmail.com>
2026-05-02 17:21:44 -0600

    Primer commit

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://wmldypredmbdpcrokdau.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtbGR5cHJlZG1iZHBjcm9rZGF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MDY2NjgsImV4cCI6MjA5MzA4MjY2OH0.hGsybjBSEBQ_l63L8fMjzpvAe6OJL3fnUE42ifkHbIs'
-
 const supabase = createClient(supabaseUrl, supabaseKey)
+async function gestionarEstudiantes() {
+  console.log('--- Iniciando aplicación ---')
+  // 1. 
+  const { error: insertError } = await supabase
+    .from('estudiantes')
+    .insert([
+      { id: 1234567, nombre: 'Nuevo Estudiante', carrera: 'Nueva Carrera' }
+    ])
 
-async function obtenerEstudiantes() {
-  const { data, error } = await supabase
+  if (insertError) {
+    console.log('Aviso inserción:', insertError.message)
+  } else {
+    console.log('¡Estudiante agregado con éxito desde el código!')
+  }
+  // 2.  
+  const { data, error: selectError } = await supabase
     .from('estudiantes')
     .select('*')
 
-  if (error) {
-    console.log('Error:', error)
+  if (selectError) {
+    console.log('Error al consultar:', selectError.message)
   } else {
-    console.log('Datos:', data)
+    console.log('--- Lista de Estudiantes en Supabase ---')
+    console.table(data) 
   }
 }
 
-obtenerEstudiantes()
-
-async function insertarEstudiante(id, nombre, carrera) {
-  const { data, error } = await supabase
-    .from('estudiantes')
-    .insert([{ id, nombre, carrera }])
-
-  if (error) {
-    console.log('Error al insertar:', error.message)
-  } else {
-    console.log('Estudiante guardado con éxito')
-  }
-}
-
-// Para usarla, solo llámala así:
-// insertarEstudiante(2, 'Mauricio Ong', 'Licenciatura en Derecho')
+gestionarEstudiantes()
